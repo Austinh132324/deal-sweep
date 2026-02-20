ed36d9535e7e211c668875d1a6b9f4be0d411dda
AustinH <ah132324@hotmail.com>
2026-02-20 12:45:26 -0600

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = process.env.GITHUB_ACTIONS ? "/deal-sweep/" : "/";
-
 export default defineConfig({
-  base,
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
